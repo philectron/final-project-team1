7de9744b6e376cb19555bd8195034185f6f45628
Phi Luu <philectron@users.noreply.github.com>
2018-06-04 23:13:35 -0700

    Make temporary routes to the navlinks

--- server.js
+++ server.js
@@ -8,8 +8,35 @@ app.engine('handlebars', exphbs({ defaultLayout: 'default' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+/**
+ * Make routes for whatever we make in the <nav> section (aka, the .navlinks)
+ * The list of links is not final yet, as we need to all agree what's on the list.
+ *
+ * These below are just my proposal and are subject to change.
+ */
+
+// "Home" page is the default page every user ends up on.
+// It should display only the goal for the day and the user's progress on that day.
 app.get('/', function(req, res, next) {
   res.status(200).render('home');
+});
+
+// "About" page talks about us and the project itself. It's a tutorial for new
+// users how to use the web app.
+app.get('/about', function(req, res, next) {
+
+});
+
+// "Stats" page will display the user's statistics over the entire course.
+app.get('/stats', function(req, res, next) {
+
+});
+
+// "Leaderboard" page. I'm not sure about this. I just want to find something to
+// integrate the database and multi-account into the web app. IMO, this is
+// expected to change.
+app.get('/leaderboard', function(req, res, next) {
+
 });
 
 app.get('*', function(req, res) {
