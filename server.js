bed1e7551d664ad96143df3cbbee7a4b0ef9dddf
Phi Luu <philectron@users.noreply.github.com>
2018-06-04 17:13:20 -0700

    Create a skeleton for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'default'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', function(req, res, next) {
+  res.status(200).render('home');
+});
+
+app.get('*', function(req, res) {
+  res.status(404).render('404');
+});
+
+app.listen(port, function() {
+  console.log('=== Server is listening on port', port);
+});
