8636f308fee8fdb86087f229729582dc367a80da
Phi Luu <philectron@users.noreply.github.com>
2018-06-13 15:56:11 -0700

    Integrate MongoDB with server.js (initial)

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var hbs = require('./helpers/handlebars')(exphbs);
+var MongoClient = require('mongodb').MongoClient;
 
-const userData = require('./userData');
+const mongoHost = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoDBName = process.env.MONGO_DB_NAME;
+const mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' +
+mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoDB = null;
+var allUsers = null;
+var currentUser = null;
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,6 +21,7 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+
 
 /**
  * Make routes for whatever we make in the <nav> section (aka, the .navlinks)
@@ -22,11 +34,11 @@ app.use(express.static('public'));
 // It should display only the goal for the day and the user's progress on that day.
 app.get('/', function(req, res, next) {
   res.status(200).render('home', {
-    userList: userData['users'],
-    user: userData['users'][0],
+    userList: allUsers,
+    user: currentUser,
     hasSidebar: true,
-    activities: userData['users'][0].activities,
-    goals: userData['users'][0].goals
+    activities: currentUser.activities,
+    goals: currentUser.goals
   });
 });
 
@@ -38,20 +50,37 @@ app.get('/about', function(req, res, next) {
 
 // "Calendar" page will show a calendar where the user's workout plans are displayed
 app.get('/calendar', function(req, res, next) {
-  res.status(200).render('calendar', {days: userData['users'][0].days});
+  res.status(200).render('calendar', {days: currentUser.days});
 });
 
 // "Leaderboard" page. I'm not sure about this. I just want to find something to
 // integrate the database and multi-account into the web app. IMO, this is
 // expected to change.
 app.get('/leaderboard', function(req, res, next) {
-  res.status(200).render('leaderboard', {users: userData['users']});
+  res.status(200).render('leaderboard', {users: allUsers});
 });
 
 app.get('*', function(req, res) {
   res.status(404).render('404');
 });
 
-app.listen(port, function() {
-  console.log('=== Server is listening on port', port);
+MongoClient.connect(mongoURL, function(err, client) {
+  if (err) {
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+
+  var username = 'Gym Rat';
+  var userCollection = mongoDB.collection('users');
+  userCollection.find().toArray(function (err, userDocs) {
+    if (err) {
+      throw err;
+    }
+    allUsers = userDocs;
+    currentUser = allUsers[0];
+
+    console.log(allUsers);
+  });
+
+  app.listen(port);
 });
